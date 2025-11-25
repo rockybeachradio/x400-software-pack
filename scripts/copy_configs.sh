2b91f94b0bbd6dc6ebce11e2315932e537231311
rockybeachradio <a.menrath@gmx.de>
2025-11-25 14:04:21 +0100

    script update

--- scripts/copy_configs.sh
+++ scripts/copy_configs.sh
@@ -169,7 +169,8 @@ ask_yn() {
 answer=$(ask_yn "This script will eventually override existing files and folders. Continue?" true)
 
 echo "whatever"
-echo "$answer"
+
+echo $answer
 
 if [[ "$answer" =~ ^[Yy]$ ]]; then
     echo "Okay, lets start ..."
